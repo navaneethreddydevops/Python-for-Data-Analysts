60076e0b8d3289efb3db1b7d13b6961c80c47fc9
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-02-21 10:57:40 -0600

    addign the yaml parser code

--- Chapter2/yamlparser/yamlparser.py
+++ Chapter2/yamlparser/yamlparser.py
@@ -1,2 +1,10 @@
 import yaml
+from pprint import pprint
 
+def yamlparser():
+    with open('playbook.yaml','r') as yamlfile:
+        ansibleplaybook = yaml.safe_load(yamlfile)
+        pprint(ansibleplaybook)
+
+if __name__ =='__main__':
+    yamlparser()
