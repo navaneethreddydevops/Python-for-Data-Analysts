1e3b75b91e0dc4613015426eb75ffbdc06c9701c
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-02-21 11:02:48 -0600

    adding the json and yaml parsers

--- Chapter2/jsonparser/policyparser.py
+++ Chapter2/jsonparser/policyparser.py
@@ -3,12 +3,20 @@ import ast
 from pprint import pprint
 
 
-def readjsonpretty():
+def readjsonfromfile():
     '''This Method will parsed the JSON in pretty format'''
     with open('policy.json', 'r') as iam_policy:
         policy = json.dumps(json.load(iam_policy))
         pprint(policy)
 
 
+def writejsontofile():
+    '''This Method will write the parsed the JSON to a file'''
+    with open('policy-write.json', 'w') as iam_policy:
+        policy = json.dumps(json.load(iam_policy))
+        pprint(policy)
+
+
 if __name__ == '__main__':
-    readjsonpretty()
+    readjsonfromfile()
+    writejsontofile()
--- Chapter2/yamlparser/yamlparser.py
+++ Chapter2/yamlparser/yamlparser.py
@@ -1,10 +1,21 @@
 import yaml
 from pprint import pprint
 
-def yamlparser():
-    with open('playbook.yaml','r') as yamlfile:
+
+def yamlreadfromfile():
+    ''' This method will read the yaml file and print it in formatted way'''
+    with open('playbook.yaml', 'r') as yamlfile:
         ansibleplaybook = yaml.safe_load(yamlfile)
         pprint(ansibleplaybook)
 
-if __name__ =='__main__':
-    yamlparser()
+
+def yamlwritetofile():
+    ''' This method will write the yaml to a file and print it in formatted way'''
+    with open('playbook-write.yaml', 'w') as yamlfile:
+        ansibleplaybook = yaml.safe_load(yamlfile)
+        pprint(ansibleplaybook)
+
+
+if __name__ == '__main__':
+    yamlreadfromfile()
+    yamlwritetofile()
