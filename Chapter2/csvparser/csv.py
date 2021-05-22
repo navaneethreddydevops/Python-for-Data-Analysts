c5d01e603e4f53b771b06d5a0f2c11ef0db25772
Navaneeth Reddy <navaneethreddydevops@gmail.com>
2021-05-22 13:18:54 -0500

    removing unwanted files

--- Chapter2/csvparser/csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-file_path = '/Users/navaneethreddy/Documents/Code/Python-for-DevOps/cars.csv'
-
-with open(file_path, newline='') as csv_file:
-    off_reader = csv.reader(csv_file , delimiter=',')
-    for _ in range(10):
-        print(next(off_reader))
